502f9277ec4418b12d44fa469d2c0f283ada1dcd
2058914895 <2058914895@qq.com>
2022-07-18 23:39:07 +0800

    Add files via upload
    initial

---
+++ LTWallet/LTWallet/LTWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTWalletApp.swift
+//  LTWallet
+//
+//  Created by lt on 2022/7/18.
+//
+
+import SwiftUI
+
+@main
+struct LTWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
